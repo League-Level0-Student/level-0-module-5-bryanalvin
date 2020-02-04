f77d1bc5010935a73a9b15f3f8b870b66b85eb18
ilabstaff <ilabstaff@172.16.116.128>
2020-02-03 17:49:14 -0800

    Only had time for snowflake today but i will do Code4life tomorrow.

--- src/_08_code_4_life/Code4Life.java
+++ src/_08_code_4_life/Code4Life.java
@@ -15,7 +15,7 @@ import javax.swing.JApplet;
 public class Code4Life {
 
 	// 1. Ask the user how many hours they spent coding this week.
-
+   
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
 
