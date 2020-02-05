ac72075257ca277e61d69f6cbeb1b82d73d53abc
ilabstaff <ilabstaff@172.16.116.128>
2020-02-04 17:30:32 -0800

    Evil Polys and Code 4life

--- src/_08_code_4_life/Code4Life.java
+++ src/_08_code_4_life/Code4Life.java
@@ -11,19 +11,33 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
+public static void main(String[] args) {
+	
 
 	// 1. Ask the user how many hours they spent coding this week.
-   
+String Cry=JOptionPane.showInputDialog("How many hours have you coded today");
+int Sofia=Integer.parseInt(Cry);
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-
+if (Sofia<=2) {
+	JOptionPane.showMessageDialog(null, "GET OF THE DANG VIDEO GAME!");
+}
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
-
+if (Sofia>=3 &&Sofia<=5) {
+	JOptionPane.showMessageDialog(null, "Your a coding master.");
+}
 	// 4. If it is more than 5, call the method below to play the Batman theme song.
+if (Sofia>5) {
+	playBatmanTheme(); {
+		
+	}
+}
 
+}
 	private static void playBatmanTheme() {
 		try {
 			AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
--- src/_99_extra/EvilPolygonsChallenge.java
+++ src/_99_extra/EvilPolygonsChallenge.java
@@ -1,4 +1,5 @@
 package _99_extra;
+
 import java.awt.Color;
 
 import javax.swing.JOptionPane;
@@ -6,24 +7,53 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class EvilPolygonsChallenge {
-	
+
+
+
+
 	public static void main(String[] args) {
-		
-		//1. Create a new Robot
 
-		//2. Set the speed to 100
+		// 1. Create a new Robot
+		Robot Dan = new Robot("batman");
+		// 2. Set the speed to 100
+		Dan.setSpeed(100);
+		int colorChoice = JOptionPane.showOptionDialog(null, "What color do you want?", "Whatever you desire?",
+				JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null,
+				new String[] { "Blue", "Red", "Black"}, 0);
 
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
-		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
-		//4. Ask the use how many polygons they want to be drawn.
-		
-		//5. Use the robot to draw the number of polygons the user requested.
-		
-		//6. Make it so your shapes do not overlap
-
-		//7. Challenge: add more colors to the Option Dialog.
-	}
+		// 3. Use if statements to check the the value of colorChoice and set the pen
+		// color accordingly
+		Dan.penDown();
+		if (colorChoice == 0) {
+			Dan.setPenColor(Color.blue);
+		}
+		if (colorChoice == 1) {
+			Dan.setPenColor(Color.red);
+		}
+		if (colorChoice == 2) {
+			Dan.setPenColor(Color.black);
+		}
+		if (colorChoice == 2) {
+			Dan.setPenColor(Color.white);
+		}
+			if (colorChoice == 2) {
+				Dan.setPenColor(Color.orange);
+			}
+				if (colorChoice == 2) {
+					Dan.setPenColor(Color.pink);
+				}
+		// 4. Ask the use how many polygons they want to be drawn.
+String Dry=JOptionPane.showInputDialog("How many polys do you want");
+int Nrg=Integer.parseInt(Dry);
+		// 5. Use the robot to draw the number of polygons the user requested.
+for (int i = 0; i < Nrg; i++) {
+	      Dan.move(90);
+	      Dan.turn(360/Nrg);
 }
+		// 6. Make it so your shapes do not overlap
 
+		// 7. Challenge: add more colors to the Option Dialog.
+
+	}
+
+			}
